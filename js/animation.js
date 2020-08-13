a4f3ae5bd953ef9bfbd4ac2e04e82df612681976
aliafarin <you@example.com>
2020-08-13 13:28:01 +0430

    improved animation

--- js/animation.js
+++ js/animation.js
@@ -7,26 +7,26 @@ const animateOnClick = (element, classToAdd) => {
 const animateOnAboutPage = (features) => {
 
   setTimeout(() => {
-    $(features[0]).addClass('slide-right');
+    $(features[0]).addClass('animate-in');
   }, 200);
 
   setTimeout(() => {
-    $(features[1]).addClass('slide-left');
+    $(features[1]).addClass('animate-in');
   }, 500);
 
   setTimeout(() => {
-    $(features[2]).addClass('slide-right');
+    $(features[2]).addClass('animate-in');
   }, 700);
 
   setTimeout(() => {
-    $(features[3]).addClass('slide-left');
+    $(features[3]).addClass('animate-in');
   }, 900);
 
   setTimeout(() => {
-    $(features[4]).addClass('slide-right');
+    $(features[4]).addClass('animate-in');
   }, 1100);
 
   setTimeout(() => {
-    $(features[5]).addClass('slide-left');
+    $(features[5]).addClass('animate-in');
   }, 1300);
 }
