47236cabfe8a088601823f78ae644e3d12f3f423
aliafarin <you@example.com>
2020-08-17 01:08:16 +0430

    added next and previous week options

--- js/calendar.js
+++ js/calendar.js
@@ -7,11 +7,6 @@ let week = date.format('dddd');
 let weekNumber = date.calendar().weekday;
 let selectedDay;
 
-//calendar top
-$('.top-week').text(week);
-$('.top-month').text(`${month} ${year}`);
-$('.top-day').text(day);
-
 const followAlongSpan = (selectedDay) => {
   let coords = selectedDay.getBoundingClientRect();
   let y = coords.top + window.scrollY;
@@ -22,19 +17,6 @@ const followAlongSpan = (selectedDay) => {
     width: "32px"
   });
 } 
-
-//selectDay
-$('.day').click(function() {
-  selectedDay = Number($(this).text());
-  $('.selected-day').removeClass('selected-day');
-  $(this).addClass('selected-day');
-  followAlongSpan($(this)[0]);
-
-  //show list for selected day
-  showList(lists, selectedDay);
-});
-
-
 
 let findMonthLimit = (monthNumber) => {
   let limit = (monthNumber <= 6) ? 31 : 30;
@@ -51,7 +33,7 @@ let findFirstWeekDay = (weekNumber, weekDay) => {
   return weekDay;
 }
 
-let addTodayClass = (dayElem) => {
+let addTodayClass = (dayElem, weekDay) => {
   if( day === weekDay ) {
     $(dayElem).addClass('today');
   } 
@@ -65,16 +47,76 @@ let findNextDay = (nextDay) => {
   return nextDay
 }
 
+let findLastDay = (lastDay) => {
+  lastDay --;
+  if(lastDay < 1) {
+    lastDay = limit;
+  }
+  return lastDay;
+}
+
+const renderDays = (weekDay) => {
+  days.forEach((dayElem) => {
+    let dayToShow = (weekDay < 10) ? `0${weekDay}` : weekDay;  
+    $(dayElem).text(dayToShow);
+    
+    addTodayClass(dayElem, weekDay);
+  
+    weekDay = findNextDay(weekDay);
+  });
+}
+
+const disableChangeWeek = (lastOrNextSpan) => {
+  ( $('.disable').length === 0 )
+    ? $(lastOrNextSpan).addClass('disable')
+    : $('.disable').removeClass('disable');
+}
+
+//calendar top
+$('.top-week').text(week);
+$('.top-month').text(`${month} ${year}`);
+$('.top-day').text(day);
+
 let weekDay = findFirstWeekDay(weekNumber, day);
 let limit = findMonthLimit(monthNumber);
 let days = [ ...$('.day') ];
 
-// render days
-days.forEach((dayElem) => {
-  let dayToShow = (weekDay <= 10) ? `0${weekDay}` : weekDay;  
-  $(dayElem).text(dayToShow);
+renderDays(weekDay);
 
-  addTodayClass(dayElem);
+//selectDay
+$('.day').click(function() {
+  selectedDay = Number($(this).text());
+  $('.selected-day').removeClass('selected-day');
+  $(this).addClass('selected-day');
+  followAlongSpan($(this)[0]);
 
-  weekDay = findNextDay(weekDay);
+  //show list for selected day
+  showList(lists, selectedDay);
+});
+
+// next last week
+$('#next, #last').click((e) => {
+  let lastOrNextSpan = $(e.target).closest('span');
+  let whichWeek = $(lastOrNextSpan).attr('id');
+
+  if( !$(lastOrNextSpan).hasClass('disable') ) {
+    disableChangeWeek(lastOrNextSpan);
+    //empty todo list
+    $('.list-items').empty();
+    $('.today').removeClass('today');
+
+    if( whichWeek === 'next' ) {
+      let days = $('.day');
+      let lastDayOfWeek = Number($(days[6]).text());
+      let nextWeek1Day = findNextDay(lastDayOfWeek);
+      renderDays(nextWeek1Day);
+    }
+    else {
+      let days = $('.day');
+      let firstDayOfWeek = Number($(days[0]).text());
+      let lastWeek7Day = findLastDay(firstDayOfWeek);
+      let lastWeek1Day = findFirstWeekDay(7, lastWeek7Day);
+      renderDays(lastWeek1Day);
+    }
+  }
 });
